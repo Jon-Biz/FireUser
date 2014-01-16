354ef5e245859cd4061f7cdf4a385eb01dcce4a1
Austin Brown <thataustin@gmail.com>
2014-01-16 15:22:43 -0800

    Cleaning up tests, consolidating bower.json files

--- test/karma-shared.conf.js
+++ test/karma-shared.conf.js
@@ -48,15 +48,15 @@ var shared = function(config) {
 };
 
 shared.files = [
-    // 'test/bower_components/jquery/jquery.js',
-    // 'test/bower_components/bootstrap/dist/js/bootstrap.js',
+    // 'bower_components/jquery/jquery.js',
+    // 'bower_components/bootstrap/dist/js/bootstrap.js',
 
-    'test/bower_components/angular/angular.js',
-    // 'test/bower_components/angular-route/angular-route.js',
+    'bower_components/angular/angular.js',
+    // 'bower_components/angular-route/angular-route.js',
 
-    'test/bower_components/angular-mocks/angular-mocks.js',
-//    'test/bower_components/firebase/firebase.js',
-//    'test/bower_components/angularfire/angularFire.js',
+    'bower_components/angular-mocks/angular-mocks.js',
+//    'bower_components/firebase/firebase.js',
+//    'bower_components/angularfire/angularFire.js',
     'fireUser.js'
   ];
 
--- test/karma-unit.conf.js
+++ test/karma-unit.conf.js
@@ -11,11 +11,11 @@ module.exports = function(config) {
   config.files = shared.files.concat([
   //extra testing code
   //
-    'test/bower_components/sinon/lib/sinon.js',
-    'test/bower_components/sinon/lib/sinon/call.js',
-    'test/bower_components/sinon/lib/sinon/spy.js',
-    'test/bower_components/sinon/lib/sinon/stub.js',
-    'test/bower_components/jasmine-sinon/lib/jasmine-sinon.js',
+    'bower_components/sinon/lib/sinon.js',
+    'bower_components/sinon/lib/sinon/call.js',
+    'bower_components/sinon/lib/sinon/spy.js',
+    'bower_components/sinon/lib/sinon/stub.js',
+    'bower_components/jasmine-sinon/lib/jasmine-sinon.js',
 
     //test files
     'test/unit/mocks/**/*.js',
