acd7676f0387e441f5df762388e3168cd53903b1
Jonathan El-Bizri <git@elbizri.com>
2014-01-27 18:02:37 -0800

    updated FireUser.login.spec.js tests

--- test/unit/services/FireUser.login.spec.js
+++ test/unit/services/FireUser.login.spec.js
@@ -5,43 +5,33 @@ describe('FirebaseRef Service', function () {
   var testUrl;
   beforeEach(function() {
 
-    var FirebaseStub = this.FirebaseStub = sinon.stub();
-    window.Firebase = function () {
-      return FirebaseStub;
-    };
+    Mocks.setupFireUser(this);
+    // mocks angularFire module
+    // creates stubs 
+    //  this.Firebase (Firebase global)
+    //  this.firebaseMock (Angular Firebase service)
+    //  this.firebaseAuthStub (Angular Firebase Auth provider)
+    //  this.firebaseAuthMock (Angular Firebase Auth returned service)
 
-    angular.module('firebase',[]);;
-    module('firebase');
+    this.firebaseAuthMock.$login = sinon.stub().returns();
 
-    var Firebase = this.Firebase = sinon.stub();
-
-    var firebaseAuthStub = this.firebaseAuthStub = sinon.stub();
-    var firebaseAuthMock = function () {return firebaseAuthStub;}
-
-    var FireUserDefault = this.FireUserDefault = {};
-    FireUserDefault.url = 'testing';
-    FireUserDefault.redirectPath = '/testPath';
-    module('fireUser', function($provide) {
-
-      var firebaseMock = function () {
-        return this;
-      }
-      
-      $provide.constant('FireUserDefault', FireUserDefault);
-      $provide.service('$firebase',firebaseMock);
-      $provide.service('$firebaseAuth',firebaseAuthMock);
-    });
   });
 
-    describe("when login() is called with a string login('github')", function() {
-      it("should call $firebase with that string", function() {
-        
-      });
-    });
-    describe("when login() is called with a object user", function() {
-      it("should call $firebase with 'password',{user.email, user.password}", function() {
-        
-      });
-    });
+  describe("when login() is called with 'test'", function() {
+    it("should call firebaseAuth.$login with 'test'", inject(function($fireUser) {
+      $fireUser.login('test')
+      expect(this.firebaseAuthMock.$login).toHaveBeenCalledWith('test');      
+      expect(this.firebaseAuthMock.$login).toHaveBeenCalled();
+    }));
+  });
+
+  describe("when login() is called with ('password',user)", function() {
+    it("should call firebaseAuth.$login with ('password',user)", inject(function($fireUser) {
+      this.user = {email:'test',password:'test'};
+
+      $fireUser.login('password',this.user)
+      expect(this.firebaseAuthMock.$login).toHaveBeenCalledWith('password',this.user);
+    }));
+  });
 
 });
