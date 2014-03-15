15622c94bc29459e780ab95ebb96f3cfc080c820
Jonathan El-Bizri <git@elbizri.com>
2014-03-15 15:19:24 -0700

    tidied up mocks

--- test/unit/mocks/Firebase.mocks.js
+++ test/unit/mocks/Firebase.mocks.js
@@ -6,22 +6,27 @@ Mocks ={
 
       Mocks.setupAngularFire();
 
-      firebaseAuthStub = Mocks.setupFirebaseAuthMock(that);
-      firebaseMock = Mocks.setupFirebaseMock(that);
+      var firebaseAuthStub = Mocks.setupFirebaseAuthMock(that);
+      var firebaseMock = Mocks.setupFirebaseMock(that);
+      var router$state = Mocks.setupRouter(that);
 
       module('fireUser', function($provide) {
-        var firebaseMock = function () {return this;}
+        $provide.service('FireUserConfig',function () {return{}})
         $provide.service('$firebase',firebaseMock);
-        $provide.service('$firebaseAuth',firebaseAuthStub);
+        $provide.service('$firebaseSimpleLogin',firebaseAuthStub);
+        $provide.service('$state',router$state);
       });
     },
     setupFirebaseJS: function (that) {
       var FirebaseStub = that.FirebaseStub = sinon.stub();
       window.Firebase = function () {return FirebaseStub;};      
     },
+    setupAngularFire:function () {
+      angular.module('firebase',[])
+      module('firebase')
+    },
     setupFirebaseAuthMock:function (that) {
-      // sets up firebaseAuthMock as Mock for methods
-      // sets up firebaseAuthStub as Stub of firebaseAuth service and returns the auth;
+
       that.firebaseAuthMock = {};
       var firebaseAuthStub = that.firebaseAuthStub = sinon.stub().returns(that.firebaseAuthMock);
 
@@ -30,8 +35,9 @@ Mocks ={
     setupFirebaseMock:function (that) {
       return (function () {return this;})
     },
-    setupAngularFire:function () {
-      angular.module('firebase',[])
-      module('firebase')
+    setupRouter:function (that) {
+      var angularUiRouter = sinon.stub();
+      return angularUiRouter;
     }
+
 }
