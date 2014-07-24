2e0c5ec05ef99f8566e8bdf80887460e39aa5146
Jon-Biz <github@elbizri.com>
2014-07-23 18:47:21 -0700

    removed hanging if statement

--- FireUser.js
+++ FireUser.js
@@ -20,7 +20,6 @@ angular.module('fireUser', ['firebase','ui.router'])
 function($rootScope, $location, $fireUser, $state, FireUserValues,waitForAuth) {
   if(FireUserValues.routing){
 
-    if(FireUserValues)
     var checked;
 
     $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
